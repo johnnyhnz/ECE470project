7125d1de6398c26bf4ab5f262529dea19d5bb921
johnnyhnz <73795558+johnnyhnz@users.noreply.github.com>
2022-11-16 23:15:29 -0600

    Add files via upload

---
+++ lab2_spawn.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import rospy
+import rospkg
+import os
+import sys
+import yaml
+import random
+from gazebo_msgs.srv import SpawnModel
+from gazebo_msgs.srv import DeleteModel
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+
+
+def spawn():
+    # Initialize rospack
+    rospack = rospkg.RosPack()
+    # Get path to yaml
+    lab2_path = rospack.get_path('lab2pkg_py')
+    yamlpath = os.path.join(lab2_path, 'scripts', 'lab2_data.yaml')
+
+    with open(yamlpath, 'r') as f:
+        try:
+            # Load the data as a dict
+            data = yaml.load(f)
+            # Load block position
+            block_xy_pos = data['block_xy_pos']
+            
+        except:
+            sys.exit()
+
+
+    # Initialize ROS pack
+    rospack = rospkg.RosPack()
+    # Get path to block
+    ur_path = rospack.get_path('ur_description')
+    block_path = os.path.join(ur_path, 'urdf', 'block.urdf')
+    block1_path = os.path.join(ur_path, 'urdf', 'block_red.urdf')
+    block2_path = os.path.join(ur_path, 'urdf', 'block_yellow.urdf')
+    block3_path = os.path.join(ur_path, 'urdf', 'block_green.urdf')
+    block_paths = [block1_path, block2_path, block3_path]
+    # Wait for service to start
+    rospy.wait_for_service('gazebo/spawn_urdf_model')
+    spawn = rospy.ServiceProxy('gazebo/spawn_urdf_model', SpawnModel)
+    delete = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+
+    # Delete previous blocks not working rn
+    #for i in range(2):
+        #block_name = 'block' + str(i+1)
+        #delete(block_name)
+
+    # 0-indexed and spawning equality
+    starting_location = 0
+    missing_block = str('n')    
+    missing_block = (missing_block == 'y')
+
+
+	# Spawn three blocks
+    if not missing_block:
+        
+        for height in range(3):
+            block_name = 'block' + str(height + 1)
+            pose = Pose(Point(block_xy_pos[starting_location][height][0], 
+                            block_xy_pos[starting_location][height][1], block_xy_pos[starting_location][height][2]),Quaternion(0, 0, 0, 0) )
+            spawn(block_name, open(block_paths[2-height], 'r').read(), 'block', pose, 'world')
+            print('blockspawn',height)    
+
+	    
+    
+
---
+++ lab2_spawn_nocall.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import rospy
+import rospkg
+import os
+import sys
+import yaml
+import random
+from gazebo_msgs.srv import SpawnModel
+from gazebo_msgs.srv import DeleteModel
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+
+
+
+
+yamlpath = 'lab2_data.yaml'
+
+if __name__ == '__main__':
+
+    # Initialize rospack
+    rospack = rospkg.RosPack()
+    # Get path to yaml
+    lab2_path = rospack.get_path('lab2pkg_py')
+    yamlpath = os.path.join(lab2_path, 'scripts', 'lab2_data.yaml')
+
+    with open(yamlpath, 'r') as f:
+        try:
+            # Load the data as a dict
+            data = yaml.load(f)
+            # Load block position
+            block_xy_pos = data['block_xy_pos']
+            
+        except:
+            sys.exit()
+
+    # Initialize ROS node
+    rospy.init_node('ur3_gazebo_spawner', anonymous=True)
+    # Initialize ROS pack
+    rospack = rospkg.RosPack()
+    # Get path to block
+    ur_path = rospack.get_path('ur_description')
+    block_path = os.path.join(ur_path, 'urdf', 'block.urdf')
+    block1_path = os.path.join(ur_path, 'urdf', 'block_red.urdf')
+    block2_path = os.path.join(ur_path, 'urdf', 'block_yellow.urdf')
+    block3_path = os.path.join(ur_path, 'urdf', 'block_green.urdf')
+    block_paths = [block1_path, block2_path, block3_path]
+    # Wait for service to start
+    rospy.wait_for_service('gazebo/spawn_urdf_model')
+    spawn = rospy.ServiceProxy('gazebo/spawn_urdf_model', SpawnModel)
+    delete = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+
+    # Delete previous blocks not working rn
+    #for i in range(2):
+        #block_name = 'block' + str(i+1)
+        #delete(block_name)
+
+    # 0-indexed and spawning equality
+    starting_location = 0
+    missing_block = str('n')    
+    missing_block = (missing_block == 'y')
+
+
+	# Spawn three blocks
+    if not missing_block:
+        
+        for height in range(3):
+            block_name = 'block' + str(height + 1)
+            pose = Pose(Point(block_xy_pos[starting_location][height][0], 
+                            block_xy_pos[starting_location][height][1], block_xy_pos[starting_location][height][2]),Quaternion(0, 0, 0, 0) )
+            spawn(block_name, open(block_paths[2-height], 'r').read(), 'block', pose, 'world')
+            print('blockspawn',height)
+
+
+	    
+    
+
